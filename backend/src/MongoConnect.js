06894eb772cc58343bf38ed29e0a41fcde68a5c5
arnaupool <arnyr@hotmail.es>
2019-09-24 19:19:36 +0200

    Cambios menores en MongoConnect.js

--- backend/src/MongoConnect.js
+++ backend/src/MongoConnect.js
@@ -3,8 +3,12 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:admin@clusterpunchi-sujlc.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
 client.connect(err => {
-  if(err)
-    console.log(err)
-  else
-    console.log("Connected to database")
+  if(err) throw err
+  console.log("Connected to database")
+  // Retrieve collection Users
+  var users = client.db("PUNCHI").collection("Users");
+  // Inserción de usuarios
+  users.insertOne({"_id" : "123456789", "nombre" : "Arnau"}).then(() => console.log(users.findOne()));
+
+  
 });
