0d672f6263bc9181a3502f8e9620ef941b970856
Fardenz <javibodi@gmail.com>
2019-09-24 18:55:01 +0200

    pushing as eslint correct

--- backend/src/MongoConnect.js
+++ backend/src/MongoConnect.js
@@ -1,10 +1,8 @@
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:admin@clusterpunchi-sujlc.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
-client.connect(err => {
-  if(err)
-    console.log(err)
-  else
-    console.log("Connected to database")
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://admin:admin@clusterpunchi-sujlc.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+  if (err) { console.log(err); } else { console.log('Connected to database'); }
 });
