057f0268c0fcbdca3f9f858cb2506379090d0a41
Paco Paredes <paco@wifialu39-88.wi-fi.upv.es>
2019-09-24 18:35:55 +0200

    Cambios menores MongoConnect muestra estado conexion

--- backend/src/MongoConnect.js
+++ backend/src/MongoConnect.js
@@ -3,7 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:admin@clusterpunchi-sujlc.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  if(err)
+    console.log(err)
+  else
+    console.log("Connected to database")
 });
