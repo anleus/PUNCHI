723145ae9facf467ca0075b402ef654149a80227
Anleus <tomas.rui598@gmail.com>
2019-10-08 10:12:13 +0200

    añadidos archivos a añadir cunado estructura de backend lista

---
+++ archivos importantes de mongo/MongoConnect.js
@@ -0,0 +1,18 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@clusterpunchi-sujlc.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+client.connect(err => {
+  var users = client.db("PUNCHI").collection("Users");
+  // Inserción de usuarios
+  users.insertOne({"_id" : "123456789", "nombre" : "Arnau"}).then(() => console.log(users.findOne()));
+
+
+});
+
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://admin:admin@clusterpunchi-sujlc.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+  if (err) { console.log(err); } else { console.log('Connected to database'); }
+});
