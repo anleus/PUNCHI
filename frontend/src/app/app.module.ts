db91236282e761c78ddc373b90fe0fe48273261b
laumecha <46852755+laumecha@users.noreply.github.com>
2019-10-09 22:57:36 +0200

    modificar frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,19 +9,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { SignListComponent } from './components/sign-list/sign-list.component';
+import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+
+
+const routes: Routes = [
+  { path: 'crearusuario', component: CrearUsuarioComponent},
+  { path: 'inicio', component: InicioComponent},
+  { path: '', redirectTo: 'inicio', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     TopBarComponent,
-    SignListComponent
+    SignListComponent,
+    CrearUsuarioComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
