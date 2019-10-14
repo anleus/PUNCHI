259def8e6f0e42971e4d13f03d95757c48a5a4b1
Anleus <tomas.rui598@gmail.com>
2019-10-15 01:04:18 +0200

    ventana de inicio empezada, de front falta que responda como toca y dejarlo rexulon, ademas de cambiar algunas cosas de como esta hecho

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { SignListComponent } from './components/sign-list/sign-list.component';
 import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HistoryTableComponent } from './history-table/history-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 const routes: Routes = [
@@ -23,17 +32,26 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     SideBarComponent,
-    TopBarComponent,
     SignListComponent,
     CrearUsuarioComponent,
-    InicioComponent
+    InicioComponent,
+    MainNavComponent,
+    HistoryTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/history-table/history-table.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { HistoryTableDataSource, HistoryTableItem } from './history-table-datasource';
+
+@Component({
+  selector: 'history-table',
+  templateUrl: './history-table.component.html',
+  styleUrls: ['./history-table.component.css']
+})
+export class HistoryTableComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatTable, {static: false}) table: MatTable<HistoryTableItem>;
+  dataSource: HistoryTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngOnInit() {
+    this.dataSource = new HistoryTableDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
