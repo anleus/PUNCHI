cb2a4bd507a503b45c7e375fa967d9a972a950c4
laumecha <46852755+laumecha@users.noreply.github.com>
2019-10-15 20:20:16 +0200

    cambiar imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,7 @@
+import { MaterialModule } from './material-module'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MatToolbarModule } from '@angular/material';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,18 +11,7 @@ import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.
 import { InicioComponent } from './components/inicio/inicio.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { HistoryTableComponent } from './history-table/history-table.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { LoginComponent } from './components/login/login.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatSelectModule } from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input'; 
 import { FichaPersonalComponent } from './components/ficha-personal/ficha-personal.component';
 import { DepartamentosComponent } from './components/departamentos/departamentos.component';
 import { AsignarDepComponent } from './components/asignar-dep/asignar-dep.component';
@@ -32,8 +20,6 @@ import { AsignarDepComponent } from './components/asignar-dep/asignar-dep.compon
 const routes: Routes = [
   { path: 'crearusuario', component: CrearUsuarioComponent},
   { path: 'inicio', component: InicioComponent},
-  { path: '', redirectTo: 'inicio', pathMatch: 'full'},
-  { path: 'login', component: LoginComponent},
   { path: 'departamentos', component: DepartamentosComponent},
   { path: 'fichapersonal', component: FichaPersonalComponent},
   { path: '', redirectTo: 'inicio', pathMatch: 'full'}
@@ -48,29 +34,17 @@ const routes: Routes = [
     InicioComponent,
     MainNavComponent,
     HistoryTableComponent,
-    LoginComponent,
     FichaPersonalComponent,
     DepartamentosComponent,
     AsignarDepComponent
   ],
   imports: [
     BrowserModule,
-    MatFormFieldModule,
+    MaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     RouterModule.forRoot(routes),
-    LayoutModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatCardModule,
-    MatSelectModule,
-    MatInputModule
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
