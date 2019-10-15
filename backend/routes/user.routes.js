71b5c83cd357ed4c26685aeb1d28f3ad6b68aea6
laumecha <46852755+laumecha@users.noreply.github.com>
2019-10-15 19:14:14 +0200

    backend

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,25 +1,13 @@
 const express = require('express');
-const app = express();
+//const app = express();
 const userFunctions = require('../controller/user.controller');
 const router = express.Router();
 const User = require('../models/users');
 //routes
-/*app.get('/user', (req, res, next) => res.json('Users'));
-app.get('/user/:id',);
-app.post('/user',userFunctions.addUser);
-app.put('/user/:id');
-app.delete('/user/:id', userFunctions.deleteUser);*/
-router.get('/users',(req, res) => {
-    res.send('holaaaaaaaa')
-});
-
-router.route('/users/:id').get((req, res) => {
-    Users.findById(req.params.id, (err, users) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(users);
-    });
-});
+router.get('/users', (req, res, next) => res.json('users'));
+router.get('/users/:id',);
+router.post('/users',userFunctions.addUser);
+router.put('/users/:id');
+router.delete('/users/:id', userFunctions.deleteUser);
 
 module.exports = router;
