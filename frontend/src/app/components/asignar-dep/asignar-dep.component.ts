b4fae528dff9b6d6bec570926dc7fa79b64d813c
karlets12 <karlitos_castall@hotmail.es>
2019-10-15 18:54:49 +0200

    adding departamentos jaja

---
+++ frontend/src/app/components/asignar-dep/asignar-dep.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asignar-dep',
+  templateUrl: './asignar-dep.component.html',
+  styleUrls: ['./asignar-dep.component.css']
+})
+export class AsignarDepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
