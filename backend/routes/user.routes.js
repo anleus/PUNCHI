a5406552ff2673ef07dc77c377b12f8b81539191
laumecha <46852755+laumecha@users.noreply.github.com>
2019-10-15 19:04:25 +0200

    servidor dice algo

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const userFunctions = require('../controller/user.controller');
-
+const router = express.Router();
+const User = require('../models/users');
 //routes
-app.get('/user', (req, res, next) => res.json('Users'));
+/*app.get('/user', (req, res, next) => res.json('Users'));
 app.get('/user/:id',);
 app.post('/user',userFunctions.addUser);
 app.put('/user/:id');
-app.delete('/user/:id', userFunctions.deleteUser);
+app.delete('/user/:id', userFunctions.deleteUser);*/
+router.get('/users',(req, res) => {
+    res.send('holaaaaaaaa')
+});
 
-module.exports = app;
+router.route('/users/:id').get((req, res) => {
+    Users.findById(req.params.id, (err, users) => {
+        if (err)
+            console.log(err);
+        else
+            res.json(users);
+    });
+});
+
+module.exports = router;
