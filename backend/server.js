fb410384ba413fb73b438f4d235ea0bef2a187ae
Paco Paredes <paco@wifialu25-64.wi-fi.upv.es>
2019-10-15 18:33:25 +0200

    Quitamos node_modules

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-
-import Users from './models/Users';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const users = require('./models/users');
+const userRoutes = require('./routes/user.routes');
+const connection = mongoose.connection;
 const app = express();
 const router = express.Router();
 
@@ -12,31 +13,13 @@ app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://admin:admin@clusterpunchi-sujlc.mongodb.net/test?retryWrites=true&w=majority');
 
-const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully!');
 });
 
-router.route('/users').get((req, res) => {
-    Users.find((err, users) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(users);
-    });
-});
-
-router.route('/users/:id').get((req, res) => {
-    Users.findById(req.params.id, (err, users) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(users);
-    });
-});
-
 app.use('/', router);
+app.use('/user',userRoutes);
 
 app.get('/', (req, res) => res.send('Hello world'));
 app.listen(4000, () => console.log('Express server running on port 4000'));
