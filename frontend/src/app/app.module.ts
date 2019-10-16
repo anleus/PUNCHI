076bd74a12beda2fe2a4533b06b0037c7ba0177c
Anleus <tomas.rui598@gmail.com>
2019-10-16 22:55:29 +0200

    hola

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,31 +1,20 @@
+import { MaterialModule } from './material-module'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MatToolbarModule } from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { SignListComponent } from './components/sign-list/sign-list.component';
+
+
 import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { HistoryTableComponent } from './history-table/history-table.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 import { FichaPersonalComponent } from './components/ficha-personal/ficha-personal.component';
 import { DepartamentosComponent } from './components/departamentos/departamentos.component';
 import { AsignarDepComponent } from './components/asignar-dep/asignar-dep.component';
-import {MatNativeDateModule} from '@angular/material/core';
-import { NgbdDatepickerPopup } from './components/datepicker-popup/datepicker-popup.component';
+
 
 const routes: Routes = [
   { path: 'crearusuario', component: CrearUsuarioComponent},
@@ -38,33 +27,20 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent,
-    SignListComponent,
     CrearUsuarioComponent,
     InicioComponent,
     MainNavComponent,
-    HistoryTableComponent,
     FichaPersonalComponent,
     DepartamentosComponent,
-    AsignarDepComponent,
-    DatepickerPopupComponent,
+    AsignarDepComponent
   ],
   imports: [
     BrowserModule,
-    MatFormFieldModule,
+    MaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     RouterModule.forRoot(routes),
-    LayoutModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatNativeDateModule,
-    MatSortModule
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
