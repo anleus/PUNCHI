ca12e02e114105e6f5f127caa4eeb6c7e64cfcc1
Paco Paredes <paco@Pacos-MBP.home>
2019-10-17 22:39:20 +0200

    Añadimos boton fichar dinamico y muestra tiempo que llevamos fichado

--- frontend/src/app/components/inicio/inicio.component.ts
+++ frontend/src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+/** 
+ *  Misteriosamente si pongo esto dentro de la clase no funciona,
+ *  de momento se quedará aqui, si, se que es mas feo que pegarle a un padre pero es lo que hay ¯\_(ツ)_/¯
+*/
+var t; 
+
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  fichando = false;
+  icon = 'play_arrow';
+  text = 'Empezar jornada';
+  class = 'nofichando';
+  tiempoFichando = 0;
+  timeLabel = '00:00:00';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleFicharState(){
+    this.fichando = !this.fichando;
+    this.icon =  !this.fichando ? 'play_arrow' : 'stop';
+    this.text = !this.fichando ? 'Empezar jornada' : 'Terminar jornada';
+    this.class = !this.fichando ? 'nofichando' : 'fichando';
+    if(this.fichando)
+      t = setInterval(() => {++this.tiempoFichando; this.updateTimeLabel()}, 1000);
+    else
+      clearInterval(t);
+  }
+
+  updateTimeLabel(){
+    var seconds = this.pad2(this.tiempoFichando % 60);
+    var minutes = this.pad2(Math.floor(this.tiempoFichando / 60));
+    var hour = this.pad2(Math.floor(this.tiempoFichando/ 3600));
+    this.timeLabel = `${hour}:${minutes}:${seconds}`;
+
+  }
+
+  pad2(number) {
+   return (number < 10 ? '0' : '') + number
+  }
 }
