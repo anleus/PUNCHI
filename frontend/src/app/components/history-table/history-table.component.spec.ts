063648c42283eca6c3767fd21c17038528730c0b
Anleus <tomas.rui598@gmail.com>
2019-10-18 09:42:23 +0200

    front de la tabla de historial donete

--- frontend/src/app/components/history-table/history-table.component.spec.ts
+++ frontend/src/app/components/history-table/history-table.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignListComponent } from './sign-list.component';
+import { HistoryTableComponent } from './history-table.component';
 
-describe('SignListComponent', () => {
-  let component: SignListComponent;
-  let fixture: ComponentFixture<SignListComponent>;
+describe('HistoryTableComponent', () => {
+  let component: HistoryTableComponent;
+  let fixture: ComponentFixture<HistoryTableComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignListComponent ]
+      declarations: [ HistoryTableComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SignListComponent);
+    fixture = TestBed.createComponent(HistoryTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
