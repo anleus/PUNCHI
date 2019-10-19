2c9324608deb8a1abce615ec8ce670091b468d96
Fardenz <javibodi@gmail.com>
2019-10-19 13:52:23 +0200

    modifying crear departamento for back end

--- backend/controller/departamento.controller.js
+++ backend/controller/departamento.controller.js
@@ -1,54 +1,61 @@
-const mongoose = require('mongoose');
-const Departamento = require('../models/Jornada');
+const mongoose = require("mongoose");
+const Departamento = require("../models/Department");
 
-const departamentFunctions = {}
+const departamentFunctions = {};
 
 departamentFunctions.getDeparamentos = async (req, res, next) => {
-    const deparamentos = await Departamento.find();
-    res.json(deparamentos);
-}
+	const deparamentos = await Departamento.find();
+	res.json(deparamentos);
+};
 
 departamentFunctions.getDepartamentoById = async (req, res, next) => {
-    const deparamentos = await Departamento.findById(req.params.id).catch(err => console.log(err));
-    if(deparamentos)
-        res.status(200).json(deparamentos);
-    else
-        res.status(404).json('Not Found');
-}
+	const deparamentos = await Departamento.findById(req.params.id).catch(err =>
+		console.log(err)
+	);
+	if (deparamentos) res.status(200).json(deparamentos);
+	else res.status(404).json("Not Found");
+};
 
-departamentFunctions.addDeparamento = async (req, res, next) => {
-    const deparamento = new Departamento({
-        users: req.body.users,
-        gestor: req.body.gestor,
-        nombre: req.body.nombre
-    })
-    deparamento.save()
-        .then(() => res.json({status: 'Departamento saved'}))
-}
+departamentFunctions.createDepartamento = async (req, res, next) => {
+	const deparamento = new Departamento({
+		users: null, // los usuarios se añaden posteriormente en otra peticion req.body.users,
+		gestor: null, //el gestor se añade posteriormente en otra peticion  req.body.gestor,
+		nombre: req.body.nombre
+	});
+	deparamento.save().then(() => res.json({ status: "Departamento saved" }));
+};
 
 departamentFunctions.updateDepartamento = async (req, res, next) => {
-    const deparamento = new Departamento({
-        users: req.body.users,
-        gestor: req.body.gestor,
-        nombre: req.body.nombre
-    })
-    Departamento.findByIdAndUpdate(req.body.id, {$set: deparamento}, {new: true})
-        .then(() => {
-            res.status(200); 
-            res.send("Deparamento updated")})
-        .catch((err) => {
-            res.status(400);
-            res.send("Bad request");});
+	const deparamento = new Departamento({
+		users: req.body.users,
+		gestor: req.body.gestor,
+		nombre: req.body.nombre
+	});
+	Departamento.findByIdAndUpdate(
+		req.body.id,
+		{ $set: deparamento },
+		{ new: true }
+	)
+		.then(() => {
+			res.status(200);
+			res.send("Deparamento updated");
+		})
+		.catch(err => {
+			res.status(400);
+			res.send("Bad request");
+		});
 };
 
 departamentFunctions.deleteDepartamento = async (req, res, next) => {
-    await Departamento.findByIdAndRemove(req.body.id)
-    .then(() => {
-        res.status(200);
-        res.json({status: 'Deparamento deleted'})})
-    .catch(() => {
-        res.status(500);
-        res.send("Request cannot be fullfilled")});
+	await Departamento.findByIdAndRemove(req.body.id)
+		.then(() => {
+			res.status(200);
+			res.json({ status: "Deparamento deleted" });
+		})
+		.catch(() => {
+			res.status(500);
+			res.send("Request cannot be fullfilled");
+		});
 };
 
 module.exports = departamentFunctions;
