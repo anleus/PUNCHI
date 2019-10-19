49a06dbdcab5312ac098c3005ef9f204488dfa39
jozevii <49403393+jozevii@users.noreply.github.com>
2019-10-19 02:06:40 +0200

    Vamos progresando con el modo admin

--- frontend/src/app/components/modificar-ficha-admin/modificar-ficha-admin.component.ts
+++ frontend/src/app/components/modificar-ficha-admin/modificar-ficha-admin.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-modificar-ficha-admin',
   templateUrl: './modificar-ficha-admin.component.html',
   styleUrls: ['./modificar-ficha-admin.component.css']
 })
+
 export class ModificarFichaAdminComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  
   }
+
+
+  /**
+ * Clase para obtener el departamento del usuario
+ */
+obtenerDepartamento(){
+  return "RRHH";  //Se debe sustituir por el acceso a la BD obteniendo el departamento del usuario loggeado
+}
+
+departamento = this.obtenerDepartamento(); 
+Modo = "Modo administrador";
+
+cambiarModo(){
+  if(this.Modo == "Modo administrador"){
+    this.Modo = "Modo normal";
+  }else{
+    this.Modo = "Modo administrador";
+  }
+}
+
+comprobarDepartamento(){
+  return this.departamento == "ee";
+}
 
 }
