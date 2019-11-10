e8dfacee0e1114ae5bb0f7229fe168249a2a503a
laumecha <46852755+laumecha@users.noreply.github.com>
2019-11-10 01:14:55 +0100

    intentando cosas

--- frontend/src/app/components/usuarios/usuarios.component.ts
+++ frontend/src/app/components/usuarios/usuarios.component.ts
@@ -8,6 +8,7 @@ import { Departamento } from 'src/app/models/departamento';
 
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { Observable } from 'rxjs';
 
 export interface PeriodicElement {
   name: string;
@@ -26,6 +27,7 @@ export interface PeriodicElement {
 
 export class UsuariosComponent implements OnInit {
   departamento: Departamento;
+  usersret = [];
   admin = false;
   gestor = false;
   logUser = this.authService.getCurrentUser();
@@ -40,10 +42,11 @@ export class UsuariosComponent implements OnInit {
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
+
   ngOnInit() {
     this.determinarUsuario();
-    if(this.admin)this.getUsersAdmin();
-    else this.getUsersGestor();
+    if (this.admin) this.getUsersAdmin();
+    else this.getUsersGestor(this.tabla);
   }
 
 
@@ -54,28 +57,49 @@ export class UsuariosComponent implements OnInit {
     };
   }
 
-  getUsersGestor() { //no sale en la tabla - falta evitar errores
+  getUsersGestor(callback) { //no sale en la tabla - falta evitar errores
+
+    /*this.departamentosService.getDepartamentoByGestor(this.logUser.source["_value"]._id).subscribe( //prueba 
+      res => {
+        var idsUsuarios = res["_id"];
+        this.userService.getUsersByDepartment(idsUsuarios).subscribe(
+          resp => {console.log(resp);}
+        );
+      });*/
+
+    this.getUsersAdmin(); //esto no funciona tampoco
     this.departamentosService.getDepartamentoByGestor(this.logUser.source["_value"]._id).subscribe(
       res => {
-        var nombre = res["nombre"];
         var idsUsuarios = res["usuarios"];
-        var users = [];
         idsUsuarios.forEach(element => {
-          if (this.logUser.source["_value"]._id == element) {
-            this.userService.getUserById(element).subscribe(
-              res => {
-                var aux;
-                aux = res;
-                aux.departamento = nombre;
-                users.push(aux);
-              }
-            );
-          }
+          this.userService.getUserById(element).subscribe(
+            resp => {
+              var aux;
+              aux = resp;
+              this.usersret.push(aux);
+              //console.log("en el bucle", JSON.stringify(this.usersret))
+
+            });
         });
-        this.dataSource = new MatTableDataSource<User>(users);
-        this.dataSource.paginator = this.paginator;
+        callback();
+        //console.log("USUARIOS:COMPONENT", JSON.stringify(this.usersret)) //se lee antes
       });
   }
+
+  tabla(){
+    console.log("Hola");
+    console.log("USUARIOS:COMPONENT", JSON.stringify(this.usersret)) //se lee antes
+    this.dataSource = new MatTableDataSource<User>(this.usersret);
+    this.dataSource.paginator = this.paginator;
+}
+  
+  addDepartmentGestor(users, nomDep) {
+    users.forEach(element => {
+      element.departamento = nomDep;
+    });
+
+  }
+
   getUsersAdmin() {
     this.userService.getAllUsers().subscribe(
       (res) => {
@@ -101,14 +125,16 @@ export class UsuariosComponent implements OnInit {
         }
       );
     });
-    this.dataSource = new MatTableDataSource<User>(users);
-    this.dataSource.paginator = this.paginator;
+    //console.log("tots", JSON.stringify(users));
+    console.log("tots", users);
+    //this.dataSource = new MatTableDataSource<User>(users);
+    //this.dataSource.paginator = this.paginator;
   }
 
   deleteUserSelected(element: User) {
     //Falta mensaje de confirmación.
-    this.userService.deleteUser(element._id).subscribe(
-      () => { window.location.reload() } //esto cambiar
-    )
+    //this.userService.deleteUser(element._id).subscribe(
+    //  () => { window.location.reload() } //esto cambiar
+    //)
   }
 }
