e79ce4a4f8012b5df079793d3ff386b1d606f71d
karlets12 <karlitos_castall@hotmail.es>
2019-11-12 02:36:52 +0100

    llista departaments

--- frontend/src/app/components/dep-list/dep-list.component.ts
+++ frontend/src/app/components/dep-list/dep-list.component.ts
@@ -1,60 +1,153 @@
-import { Component, OnInit, NgModule, ViewChild } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { SelectionModel } from '@angular/cdk/collections';
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router'
+import { DepartamentosService } from '../../services/departamentos.service'
+import { AuthenticationService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { User } from 'src/app/models/users';
+import { Departamento } from 'src/app/models/departamento';
 
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import { UserService } from 'src/app/services/user.service';
-import { User } from 'src/app/models/users';
-import { htmlAstToRender3Ast } from '@angular/compiler/src/render3/r3_template_transform';
-import { DepartamentosService } from 'src/app/services/departamentos.service';
-import { Departamento } from 'src/app/models/departamento';
-import { DepartamentosComponent } from '../departamentos/departamentos.component';
+import { Observable } from 'rxjs';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 @Component({
   selector: 'app-dep-list',
   templateUrl: './dep-list.component.html',
   styleUrls: ['./dep-list.component.css'],
-  providers: [DepartamentosService]
 })
-@NgModule({
-  declarations: [],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClient,
-    HttpClientModule,
-    HttpHeaders,
-    UserService,
-  ],
-  providers: [DepartamentosService],
-})
+
 export class DepListComponent implements OnInit {
-  displayedColumns = ['Nombre','Gestor','select']
+  departamento: Departamento;
+  deparset = [];
+  admin = false;
+  gestor = false;
+  logUser = this.authService.getCurrentUser();
+ 
   dataSource = new MatTableDataSource();
-  selection = new SelectionModel<User>(true, []);
-  constructor(private departamentosService: DepartamentosService) { }
-
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  displayedColumns: string[] = ['nombre', 'gestor', 'select'];
   
-  deleteDepartamentoSelected(element: Departamento) {
-    this.departamentosService.deleteDept(element._id).subscribe(
-      () => {window.location.reload()}
-    )
-  }
+ 
+  constructor(private departamentosService: DepartamentosService, private router: Router,
+    private authService: AuthenticationService, private userService: UserService) { }
+
+    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+    ngOnInit() {
+      //this.determinarUsuario();
+      //if (this.admin) this.getDeptAdmin();
+      //else this.getUsersGestor();
+      this.getDeptAdmin();
+    }
   
-  ngOnInit() {
-    this.departamentosService.getDepartamentos().subscribe(
-      (resp) => {
-        this.dataSource = new MatTableDataSource<Departamento>(resp);
-        this.dataSource.paginator = this.paginator;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+  
+    determinarUsuario() {
+      if (this.logUser.source["_value"] != null) {
+        this.admin = this.logUser.source["_value"].admin;
+        this.gestor = this.logUser.source["_value"].gestor;
+      };
+    }
+  
+    //Mostrar usuarios para gestor
+    getUsersGestor() {
+      this.departamentosService.getDepartamentos().subscribe(
+        res => {
+          if (res != null) {
+            var nombreDepartamento = res["nombre"];
+            var idsUsuarios = res["usuarios"];
+            var numberUsers = idsUsuarios.length;
+            var auxnumber = 1;
+            var users;
+            idsUsuarios.forEach(element => {
+              this.userService.getUserById(element).subscribe(
+                resp => {
+                  if (resp != null) {
+                    var aux;
+                    aux = resp;
+                    this.deparset.push(aux);
+                    if (auxnumber == numberUsers) {
+                      this.addDepartmentGestor(this.deparset, nombreDepartamento);
+                    }
+                  }
+                  else { console.log("usuario no existente, ALERTA");}
+                  auxnumber++;
+  
+                });
+            });
+          } else { console.log("no es responsable de ningún deparatmento"); } //cambiar por alerta
+        });
+    }
+    addDepartmentGestor(users, nomDep) {
+      users.forEach(element => {
+        element.departamento = nomDep;
+      });
+      this.dataSource = new MatTableDataSource<User>(users);
+      this.dataSource.paginator = this.paginator;
+    }
+  
+    //Mostrar personas para admin
+  
+    getDeptAdmin() {
+      this.departamentosService.getDepartamentos().subscribe(
+        (res) => {
+          this.addDepartment(res);
+          //console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  
+    addDepartment(users) {
+      console.log("add departemnt users null?", users);
+      users.forEach(element => {
+        this.departamentosService.getDepartamentoByUser(element._id).subscribe(
+          res => {
+            if (res[0] != null) {
+              element.departamento = res[0].nombre;
+            }
+            else {
+              element.departamento = "No pertenece a ningún departamento"
+            }
+          }
+        );
+      });
+      console.log("tots", users);
+      this.dataSource = new MatTableDataSource<User>(users);
+      this.dataSource.paginator = this.paginator;
+    }
+  
+    deleteDepSelected(element: Departamento) {
+      //Falta mensaje de confirmación.
+      //this.userService.deleteUser(element._id).subscribe(
+      //  () => { window.location.reload() } //esto cambiar
+      //)
+/*       new MaterialAlertDialogBuilder(context, R.style.ThemeOverlay_MaterialComponents_MaterialAlertDialog_Centered)
+      .setTitle("Title")
+      .setMessage("Message")
+      .setPositiveButton("Accept", () => {      this.departamentosService.deleteDept(element._id).subscribe(
+        () => {window.location.reload()}
+      )}); */
+
+    }
+  
+    editDepSelected(element: Departamento){
+      
+    }
+
+    editDepResponsibleSelected(element: Departamento) {
+
+    }
+
+    gotoDrag() {
+
+    }
   }
 
-}
+
