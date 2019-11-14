fe628f5a35006a9be169162b5b6a23fbaa808f26
jozevii <49403393+jozevii@users.noreply.github.com>
2019-11-14 19:02:14 +0100

    Vacation e incidencias

--- frontend/src/app/components/incidencias/incidencias.component.ts
+++ frontend/src/app/components/incidencias/incidencias.component.ts
@@ -5,7 +5,11 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { Observable } from 'rxjs';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
-
+import { UserService } from "src/app/services/user.service";
+import { VacationService } from "src/app/services/vacation.service";
+import { AuthenticationService } from "src/app/services/auth.service";
+import { User } from 'src/app/models/users';
+import { Vacation } from 'src/app/models/Vacation';
 
 @Component({
   selector: 'app-incidencias',
@@ -13,15 +17,27 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dial
   styleUrls: ['./incidencias.component.css']
 })
 export class IncidenciasComponent implements OnInit {
-
+  usuarioLogueado: User;
+  vacation: Vacation;
+  vacacionesUsuario: Date[];
   dataSource = new MatTableDataSource();
   displayedColumns: string[] = ['usuario', 'asunto', 'mensaje', 'estado'];
 
-  constructor( private router: Router) { 
+  constructor( 
+    private router: Router,
+    private userService: UserService,
+    private authService: AuthenticationService,
+    private vacationService: VacationService) { 
   }
 
 
   ngOnInit() {
+    this.authService.getCurrentUser().subscribe(user => (this.usuarioLogueado = user));
+    console.log("holaa");
+    console.log(this.usuarioLogueado);
+    //this.vacationService.getVacationByUsername(this.usuarioLogueado._id).then(vac=> if(pending==undefined){}else{(this.vacation = vac)});
+    //console.log("hollaaa" + this.vacation.pending);
+    //this.vacacionesUsuario = this.vacation.pending;
   }
 
 }
